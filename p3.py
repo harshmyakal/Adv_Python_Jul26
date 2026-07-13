42c8b32047712b341af3b8d598768dfd65ae8d80
Harsh <lalithababumyakal@gmail.com>
2026-07-13 18:38:40 +0530

    closure  vs class

---
+++ p3.py
@@ -0,0 +1,22 @@
+#closure  vs class
+def make_counter():
+    count = 0
+    def increment():
+        nonlocal count
+        count += 1
+        return count
+    return increment
+counter = make_counter()
+print(counter())  
+print(counter())  
+
+class Counter:
+    def __init__(self):
+        self.count = 0
+
+    def increment(self):
+        self.count += 1
+        return self.count
+counter_obj = Counter()
+print(counter_obj.increment())
+print(counter_obj.increment())
